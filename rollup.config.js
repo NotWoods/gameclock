e4765bd8e1c1eab234549240585591669a2a0fe2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-31 07:09:14 +0000

    Deploy NotWoods/gameclock to github.com/NotWoods/gameclock.git:gh-pages

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import { terser } from 'rollup-plugin-terser';
+import { absolutePath } from './lib/absolute-path.js';
+
+/** @type {import('rollup').RollupOptions} */
+const clockBundle = {
+  input: 'src/clock.js',
+  inlineDynamicImports: true,
+  output: {
+    file: 'src/clock.bundle.js',
+    format: 'iife',
+  },
+  plugins: [absolutePath(), terser()],
+};
+
+/** @type {import('rollup').RollupOptions} */
+const clockMin = {
+  input: 'src/clock.js',
+  inlineDynamicImports: false,
+  external: ['../web_modules/insights-js.js'],
+  output: {
+    file: 'src/clock.js',
+    format: 'esm',
+  },
+  plugins: [terser()],
+};
+
+export default [clockBundle, clockMin];
---
+++ workbox-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  cacheId: 'gameclock',
+  globDirectory: '.',
+  globPatterns: [
+    'index.html',
+    'src/**/*.js',
+    'web_modules/*.js',
+    'style/**/*.css',
+    'icons/**/*.{png,svg}',
+  ],
+  globIgnores: ['**/node_modules/**', '**/sw.js'],
+  swDest: 'sw.js',
+  ignoreURLParametersMatching: [/fbclid/],
+};
